52ef24b0c981f59620c7c5a27606afefd47793a6
platinumwireless <shaimaa@sulfah.com>
2024-02-05 22:29:05 +0200

    ScreenCaptureCallback on android 14 only

--- android/app/src/main/kotlin/com/example/screenshot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/screenshot/MainActivity.kt
@@ -1,40 +1,81 @@
 package com.example.screenshot
 
-
-import android.app.Activity
-import android.view.WindowManager
-import androidx.annotation.NonNull
+import android.annotation.SuppressLint
+import android.app.Activity.ScreenCaptureCallback
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
-    //    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-//        super.configureFlutterEngine(flutterEngine)
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val sound: Uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.packageName  + "/raw/alert")
-//            val mChannel = NotificationChannel("app_alerts", "app_alerts_2", NotificationManager.IMPORTANCE_HIGH)
-//            val audioAttributes: AudioAttributes = AudioAttributes.Builder()
-//                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-//                .setUsage(AudioAttributes.USAGE_ALARM)
-//                .build()
-//            mChannel.setSound(sound , audioAttributes)
-//            mChannel.description = "Important app Notifications"
-//            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-//            notificationManager.createNotificationChannel(mChannel)
-//        }
-//    }
-    private val CHANNEL = "flutter.native/helper"
+@SuppressLint("NewApi")
+class MainActivity : FlutterActivity() {
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-
-            if(call.method == "preventScreenshots") {
-                activity.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-                result.success("Hello from Kotlin!")
-            } else {
-                result.notImplemented()
-            }
-        }
+    private val screenCaptureCallback = ScreenCaptureCallback {
+        _invoke()
     }
+
+    private fun _invoke() {
+        // Inside your activity or fragment
+      MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, "your_channel_name").invokeMethod("")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerScreenCaptureCallback(mainExecutor, screenCaptureCallback)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterScreenCaptureCallback(screenCaptureCallback)
+    }
+
 }
+
+//import android.annotation.SuppressLint
+//import androidx.activity.ComponentActivity
+//import io.flutter.embedding.android.FlutterActivity
+//
+//@SuppressLint("NewApi")
+//class MainActivity: FlutterActivity() {
+//
+//    private val screenCaptureCallback = ScreenCaptureCallback {
+//        // mainViewModel.screenshotValue()
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        registerScreenCaptureCallback(mainExecutor, screenCaptureCallback)
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        unregisterScreenCaptureCallback(screenCaptureCallback)
+//    }
+//    //    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+////        super.configureFlutterEngine(flutterEngine)
+////        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+////            val sound: Uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.packageName  + "/raw/alert")
+////            val mChannel = NotificationChannel("app_alerts", "app_alerts_2", NotificationManager.IMPORTANCE_HIGH)
+////            val audioAttributes: AudioAttributes = AudioAttributes.Builder()
+////                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+////                .setUsage(AudioAttributes.USAGE_ALARM)
+////                .build()
+////            mChannel.setSound(sound , audioAttributes)
+////            mChannel.description = "Important app Notifications"
+////            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+////            notificationManager.createNotificationChannel(mChannel)
+////        }
+////    }
+//
+////    private val CHANNEL = "flutter.native/helper"
+////
+////    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+////        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+////
+////            if(call.method == "preventScreenshots") {
+////                activity.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+////                result.success("Hello from Kotlin!")
+////            } else {
+////                result.notImplemented()
+////            }
+////        }
+////    }
+//}
